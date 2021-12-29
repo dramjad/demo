2940cdfb537c875eb89fcb6c0db8592a3af60da0
dramjad <amjadfarooq@uet.edu.pk>
2021-12-29 13:27:15 +0500

    abc

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      padding: EdgeInsets.all(50),
-      child: const Text('Welcome'),
-    ));
+      appBar: AppBar(
+        leading: const Icon(Icons.account_circle_rounded),
+        // leadingWidth: 200,
+        title: const Text('Welcome'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () {},
+          ),
+          // IconButton(
+          //   icon: const Icon(Icons.bluetooth),
+          //   onPressed: () {},
+          // ),
+          // IconButton(
+          //   icon: const Icon(Icons.bluetooth),
+          //   onPressed: () {},
+          // ),
+          // IconButton(
+          //   icon: const Icon(Icons.bluetooth),
+          //   onPressed: () {},
+          // ),
+          // IconButton(
+          //   icon: const Icon(Icons.bluetooth),
+          //   onPressed: () {},
+          // ),
+        ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Push'),
+          onPressed: () => Navigator.push(context, MaterialPageRoute(
+            builder: (context) {
+              return SecondPage();
+            },
+          )),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
